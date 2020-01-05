c7e70f9e0ceda35e961f1327e1668798cadcdecc
Alex Barilaro <a_barilaro@pm.me>
2020-01-05 13:12:01 +1100

    Add tag index page building, TODO for raw filename processing

--- postmaker.sh
+++ postmaker.sh
@@ -1,10 +1,11 @@
 # Set up filenames and locations
-POST_TEMPLATE=post_template.html
-INDEX_TEMPLATE=index_template.html
-ENTRY_TEMPLATE=entry_template.html
 RAW_DIR=$PWD/raw/
 POSTS_DIR=$PWD/posts/
 TAGS_DIR=$PWD/tags/
+TEMPLATE_DIR=$PWD/
+POST_TEMPLATE=${TEMPLATE_DIR}post_template.html
+INDEX_TEMPLATE=${TEMPLATE_DIR}index_template.html
+ENTRY_TEMPLATE=${TEMPLATE_DIR}entry_template.html
 
 # Check that template files exist
 echo -n 'Finding template files...'
@@ -82,7 +83,12 @@ do
 		# Make a directory for the tag, if not already present
 		mkdir --parents $TAGS_DIR$tag
 
-		# TODO: Append this post's title and description to a list of all posts with this tag
+		# Append this post's title and description to a list of all posts with this tag
+		POST_LINK=/$(basename $POSTS_DIR)/$(basename $f).html
+		desc=$(sed -n '2p' $f | sed 's_[&\]_\\&_g')
+		# TODO: Fix underscores in raw file filename breaking sed command
+		entry=$(sed "s_!POSTFILENAME!_${POST_LINK}_" $ENTRY_TEMPLATE | sed "s_!POSTNAME!_${title}_" | sed "s_!POSTDESC!_${desc}_")
+		echo $entry >> $TAGS_DIR$tag/index.html
 	done
 	echo -e "\e[1;32m Done.\e[0m"
 
